97e4cc9cb5fb538e3b7f561abcb3ac033735fe5e
msmaltsev <maltsevmark@gmail.com>
2017-06-08 18:19:17 +0300

    first commit

---
+++ open_as.py
@@ -0,0 +1,23 @@
+# /usr/local/bin/python3
+# -*- coding: utf8 -*-
+
+import os, random
+
+def choose(f):
+    f = open(f, encoding='utf8').read().split('\n')
+    c = random.choice(f)
+    return c
+
+def runAppleScript(f, *args):
+    f = open(f, encoding='utf8').read()
+    f = f%(radio, traffic, weather)
+    os.system("osascript -e '%s'"%f)
+
+if __name__ == '__main__':
+
+    cwd = '/Users/sergejmaltcev/Desktop/scripts/'
+
+    radio = choose(cwd+'radio.txt')
+    traffic = choose(cwd+'traffic.txt')
+    weather = choose(cwd+'weather.txt')
+    runAppleScript(cwd+'applescript_budilnick.txt', radio, weather, traffic)
